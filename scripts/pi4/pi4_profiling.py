4771d250cb27a5796f4994e7cd73eccb9a892632
Matthew Barondeau <mebarondeau@utexas.edu>
2024-03-05 13:41:01 -0600

    Add frequency setting for raspberry pi

--- scripts/pi4/pi4_profiling.py
+++ scripts/pi4/pi4_profiling.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-
-MAX_PERF_EVENTS = 4
+import time
 
 if __name__ == "__main__":
     benchmarks = [600, 602, 605, 620, 623, 625, 631, 641, 648, 657]
@@ -12,16 +11,22 @@ if __name__ == "__main__":
 
     sample_period = [1, 2, 4, 10]
     cpus = [0]
+    freqs = ['1.80GHz', '1.00GHz']
 
+
+    time.sleep(30)
 
     for cpu in cpus:
-        for epoch in sample_period:
-            for benchmark in benchmarks:
-                for events in perf_events:
-                    command  = "taskset --cpu-list " + str(cpu)
-                    command += ' perf stat -I ' + str(epoch)
-                    command += ' -e ' + str(events)
-                    command += ' -o cpu_' + str(cpu) + '_epoch_' + str(epoch) + '_' + str(benchmark) + '_' + str(events)
-                    command += ' -x,'
-                    command += " runcpu --config=matthew-1cpu " + str(benchmark)
-                    os.system(command)
+        for freq in freqs:
+            for epoch in sample_period:
+                for benchmark in benchmarks:
+                    for events in perf_events:
+                        command  = "taskset --cpu-list " + str(cpu)
+                        command += ' perf stat -I ' + str(epoch)
+                        command += ' -e ' + str(events)
+                        command += ' -o cpu_' + str(cpu) + '_freq_' + str(freq) + '_epoch_' + str(epoch) + '_' + str(benchmark) + '_' + str(events)
+                        command += ' -x,'
+                        command += " runcpu --config=matthew-1cpu " + str(benchmark)
+                        os.system('cpupower frequency-set --max ' + str(freq))
+                        os.system('cpupower frequency-set -g performance')
+                        os.system(command)
