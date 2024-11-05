91dc807128e5a38fa5d3b86595e109e20309c08d
matthewbarondeau <mebarondeau@utexas.edu>
2024-11-05 08:47:47 -0600

    Fix data collection script directory

--- scripts/data_collection/x86_desktop_homogeneous/profiling.py
+++ scripts/data_collection/x86_desktop_homogeneous/profiling.py
@@ -6,27 +6,24 @@ import time
 if __name__ == "__main__":
     benchmarks = [
                 500, 502, 505, 520, 523, 525, 531, 541, 548, 557,
-                503, 507, 508, 510, 511, 519, 521, 526, 527, 538, 544, 549, 554]
-
+                503, 507, 508, 510, 511, 519, 521, 527, 538, 544, 549, 554]
     perf_events = [
-                "instructions,cpu-cycles,br_misp_retired.all_branches,br_inst_retired.all_branches",
-                "instructions,L1-dcache-loads-misses,L1-dcache-loads,L1-dcache-stores",
-                "instructions,branch-load-misses,branch-loads,dTLB-stores",
+                "instructions,cpu-cycles,br_inst_retired.all_branches_pebs,br_misp_retired.all_branches_pebs",
+                "instructions,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores",
+                "instructions,L1-icache-load-misses,LLC-load-misses,LLC-loads",
+                "instructions,LLC-store-misses,LLC-stores,fp_arith_inst_retired.scalar_single",
                 "instructions,dTLB-load-misses,dTLB-loads,dTLB-store-misses",
-                "instructions,iTLB-load-misses,l2_request.all,l2_request.miss",
-                "instructions,mem_store_retired.l2_hit,LLC-load-misses",
-                "instructions,LLC-loads,LLC-store-misses,LLC-stores",
-                "instructions,mem-loads,mem-stores,context-switches"
+                "instructions,dTLB-stores,iTLB-load-misses,iTLB-loads",
+                "instructions,mem_load_retired.l1_hit,mem_load_retired.l1_miss,mem_load_retired.l2_hit",
+                "instructions,mem_load_retired.l2_miss,mem_load_retired.l3_hit,mem_load_retired.l3_miss",
                 ]
 
     sample_period = [10]
     cpus = [0]
-    freqs = ['3.00GHz']
+    freqs = ['1.50GHz']
 
     for cpu in cpus:
         for freq in freqs:
-            print('cpupower frequency-set --max ' + str(freq))
-            print('cpupower frequency-set -g performance')
             for epoch in sample_period:
                 for benchmark in benchmarks:
                     count = 0
@@ -36,7 +33,7 @@ if __name__ == "__main__":
                         command += ' -e ' + str(events)
                         command += ' -o cpu_' + str(cpu) + '_freq_' + str(freq) + '_epoch_' + str(epoch) + '_' + str(benchmark) + '_' + str(count)
                         command += ' -x,'
-                        command += " runcpu --config=matthew-1cpu " + str(benchmark)
+                        command += " runcpu --config=matthew-2 " + str(benchmark)
                         print(command)
                         count += 1
 
