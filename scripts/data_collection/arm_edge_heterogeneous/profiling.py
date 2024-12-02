f18eaee2060aa4f6ff04c6732155b7ac649485d7
Matthew Barondeau <mebarondeau@utexas.edu>
2024-12-02 14:00:13 -0600

    Add final arm_edge_heterogeneous data collection scripts

--- scripts/data_collection/arm_edge_heterogeneous/profiling.py
+++ scripts/data_collection/arm_edge_heterogeneous/profiling.py
@@ -6,7 +6,7 @@ import time
 if __name__ == "__main__":
     benchmarks = [
                 500, 502, 505, 520, 523, 525, 531, 541, 548, 557,
-                503, 507, 508, 510, 511, 519, 521, 526, 527, 538, 544, 549, 554]
+                503, 507, 508, 510, 511, 519, 521, 527, 538, 544, 549, 554]
     perf_events = [
                 "instructions,cpu-cycles",
                 "instructions,armv8_pmuv3/stall_backend/",
@@ -37,31 +37,42 @@ if __name__ == "__main__":
                 "instructions,armv8_pmuv3/l3d_cache_wb/",
                 "instructions,armv8_pmuv3/mem_access/"
                 ]
-#br_pred,br_mis_pred",
-#        "L1-dcache-load-misses,L1-dcache-loads,L1-icache-load-misses,L1-icache-loads",
-#        "l2d_cache,l2d_cache_rd,l3d_cache,l3d_cache_rd",
-#        "vfp_spec,mem_access,mem_access_rd",
-#        "stall_backend,stall_frontend"
-#    ]
 
-    sample_period = [10]
-    cpus = [7]
-    freqs = ['1.52GHz']
+    for benchmark in benchmarks:
+        count = 0
+        for events in perf_events:
+            command  = "taskset --cpu-list 0"
+            command += ' perf stat -I 10'
+            command += ' -e ' + str(events)
+            command += ' -o arm_edge_InO_1.5GHz_' + str(benchmark) + '_' + str(count)
+            command += ' -x,'
+            command += " runcpu --config=matthew-1cpu " + str(benchmark)
+            print(command)
+            count += 1
 
-    for cpu in cpus:
-        for freq in freqs:
-            for epoch in sample_period:
-                for benchmark in benchmarks:
-                    count = 0
-                    for events in perf_events:
-                        command  = "taskset --cpu-list " + str(cpu)
-                        command += ' perf stat -I ' + str(epoch)
-                        command += ' -e ' + str(events)
-                        command += ' -o cpu_' + str(cpu) + '_freq_' + str(freq) + '_epoch_' + str(epoch) + '_' + str(benchmark) + '_' + str(count)
-                        command += ' -x,'
-                        command += " runcpu --config=matthew-1cpu " + str(benchmark)
-                        print(command)
-                        count += 1
+    for benchmark in benchmarks:
+        count = 0
+        for events in perf_events:
+            command  = "taskset --cpu-list 4"
+            command += ' perf stat -I 10'
+            command += ' -e ' + str(events)
+            command += ' -o arm_edge_OOO_1.5GHz_' + str(benchmark) + '_' + str(count)
+            command += ' -x,'
+            command += " runcpu --config=matthew-1cpu " + str(benchmark)
+            print(command)
+            count += 1
+
+    for benchmark in benchmarks:
+        count = 0
+        for events in perf_events:
+            command  = "taskset --cpu-list 7"
+            command += ' perf stat -I 10'
+            command += ' -e ' + str(events)
+            command += ' -o arm_edge_OOO_cache_1.5GHz_' + str(benchmark) + '_' + str(count)
+            command += ' -x,'
+            command += " runcpu --config=matthew-1cpu " + str(benchmark)
+            print(command)
+            count += 1
 
 
 
