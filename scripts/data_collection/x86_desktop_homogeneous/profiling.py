fd5bb7fc5f7eece47a20daaf48fa505a79e95875
Matthew Barondeau <mebarondeau@utexas.edu>
2025-01-06 09:16:53 -0600

    Update nu data collection script

--- scripts/data_collection/x86_desktop_homogeneous/profiling.py
+++ scripts/data_collection/x86_desktop_homogeneous/profiling.py
@@ -11,11 +11,20 @@ if __name__ == "__main__":
                 "instructions,cpu-cycles,br_inst_retired.all_branches_pebs,br_misp_retired.all_branches_pebs",
                 "instructions,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores",
                 "instructions,L1-icache-load-misses,LLC-load-misses,LLC-loads",
-                "instructions,LLC-store-misses,LLC-stores,fp_arith_inst_retired.scalar_single",
+                "instructions,LLC-store-misses,LLC-stores,fp_assist.any",
                 "instructions,dTLB-load-misses,dTLB-loads,dTLB-store-misses",
                 "instructions,dTLB-stores,iTLB-load-misses,iTLB-loads",
+                "instructions,l1d.replacement,l2_lines_in.all,l2_rqsts.all_pf",
+                "instructions,l2_rqsts.all_code_rd,l2_rqsts.code_rd_hit,l2_rqsts.code_rd_miss",
+                "instructions,l2_rqsts.all_demand_miss,l2_rqsts.all_demand_references,l2_rqsts.miss",
+                "instructions,mem_inst_retired.all_loads,mem_inst_retired.all_stores,mem_load_retired.fb_hit",
                 "instructions,mem_load_retired.l1_hit,mem_load_retired.l1_miss,mem_load_retired.l2_hit",
                 "instructions,mem_load_retired.l2_miss,mem_load_retired.l3_hit,mem_load_retired.l3_miss",
+                "instructions,fp_arith_inst_retired.128b_packed_double,fp_arith_inst_retired.128b_packed_single,fp_arith_inst_retired.256b_packed_double",
+                "instructions,fp_arith_inst_retired.256b_packed_single,fp_arith_inst_retired.scalar_double,fp_arith_inst_retired.scalar_single",
+                "instructions,frontend_retired.itlb_miss,frontend_retired.l1i_miss,frontend_retired.l2_miss",
+                "instructions,cycle_activity.stalls_total,cycle_activity.stalls_mem_any,cycle_activity.cycles_mem_any",
+                "instructions,uops_executed.core,uops_issued.any,uops_retired.stall_cycles"
                 ]
 
     sample_period = [10]
